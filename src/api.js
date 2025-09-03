7c389e0fb70f9bb65ac2c0739bf18f882775650b
MarciusMcflay <jhorge.marcius@gmail.com>
2025-09-03 17:28:42 -0300

    api feita

---
+++ src/api.js
@@ -0,0 +1,35 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+
+import userRoutes from './routes/user';
+import habitRoutes from './routes/habit';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/users', userRoutes);
+app.use('/habits', habitRoutes);
+
+// Middleware de erro simples
+app.use((err, _req, res, _next) => {
+    console.error(err);
+    if (err.code === 'P2002'){
+        return res.status(409).json({
+            error: 'Registro duplicado (unique)' 
+        });
+    } 
+    if (err.code === 'P2025'){
+         return res.status(404).json({
+            error: 'Registro não encontrado' 
+        });
+    }
+    res.status(500).json({ error: 'Erro interno' });
+});
+  
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+  
+  
+
---
+++ src/controllers/user.js
@@ -0,0 +1,36 @@
+import prisma from '../prisma';
+
+//C - CREATE, INSERT, POST, SET, STORE
+
+//asincrona nome_da_função(recebendo, responder, proximo)
+export const UserController = {
+    async store(req, res, next){
+        try{
+            const { name, cpf, email, pass, phone, signature } = req.body;
+
+            const u = await prisma.user.create({
+                data: { 
+                    name, 
+                    cpf, 
+                    email, 
+                    pass, 
+                    phone, 
+                    signature
+                }
+            });
+
+            //respondendo 201-criado encapsulando_no_formato_json(u)
+            res.status(201).json(u);
+        }catch(err){
+            next(err);
+        }
+    }
+}
+
+
+
+
+
+
+
+
