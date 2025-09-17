dbfdaada3fbb93f77258c7e71403b7bc9202053d
MarciusMcflay <jhorge.marcius@gmail.com>
2025-09-17 17:44:18 -0300

    passado o delete, ficou de exercicios fazerem o route e o http

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,9 +1,10 @@
 import prisma from '../prisma.js';
 
-//C - CREATE, INSERT, POST, SET, STORE
+
 
 //asincrona nome_da_função(recebendo, responder, proximo)
 export const UserController = {
+    //C - CREATE, INSERT, POST, SET, STORE
     async store(req, res, next){
         try{
             const { name, cpf, email, pass, phone, signature } = req.body;
@@ -25,18 +26,50 @@ export const UserController = {
             next(err);
         }
     },
+
+    //R - READ, SELECT, GET, findMany
     async index(req, res, next){
         
         let query = {}
 
         if (req.query.name) query = {name: req.query.name}
         if (req.query.email) query = {email: req.query.email}
+        if (req.query.cpf) query = {cpf: req.query.email}
 
         const users = await prisma.user.findMany({
             where: query
         })
 
         res.status(200).json(users)
+    },
+
+    // R - READ, SELECT, GET
+    async show(req, res, _next) {
+        try {
+            const id = Number(req.params.id);
+
+            const u = await prisma.user.findUniqueOrThrow({
+                where: { id }
+            });
+
+            res.status(200).json(u);
+        } catch (err) {
+            res.status(404).json({ error: "Usuário não encontrado" });
+        }
+    },
+
+    async del(req, res, _next){
+        try {
+            const id = Number(req.params.id);
+
+            const u = await prisma.user.delete({
+                where: { id }
+            });
+
+            res.status(200).json(u);
+        } catch (err) {
+            res.status(404).json({ error: "Usuário não encontrado" });
+        }
     }
 }
 
--- src/routes/user.js
+++ src/routes/user.js
@@ -4,6 +4,7 @@ import { UserController } from '../controllers/user.js'
 const route = Router();
 
 route.post('/', UserController.store);
+route.get('/:id', UserController.show);
 route.get('/', UserController.index);
 
 export default route;
